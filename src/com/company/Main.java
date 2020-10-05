2684a22b44c27d1792f53df10b05d32b7ae1c5fc
Walid <walidhaddoury@outlook.fr>
2020-10-05 15:31:13 +0200

    fin du TP 01

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,23 @@ package com.company;
 
 public class Main {
 
+
+
     public static void main(String[] args) {
-	// write your code here
+
+        int [] scores = new int[7];
+        scores[0] = 19;
+        scores[1] = 11;
+        scores[2] = 16;
+        scores[3] = 20;
+        scores[4] = 9;
+        scores[5] = 6;
+        scores[6] = 10;
+
+        int moyenne = 0;
+        for (int x = 0; x < scores.length; x++){
+            moyenne = moyenne + scores[x];
+        }
+        System.out.println(moyenne / scores.length);
     }
 }
